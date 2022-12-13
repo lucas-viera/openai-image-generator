e30aae5a2fd1a4b3a4282b08e0a5a1c0f7b37f61
Lucas Viera <lucasviera22@gmail.com>
2022-12-13 20:08:38 -0300

    Updated controller to accept url encoded content at request body

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
+//Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
