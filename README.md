aee8c6d4e35d99b69d709ed06acfc612a82f0202
Lucas Viera <lucasviera22@gmail.com>
2022-12-13 20:11:46 -0300

    Updated gitignore after clean cache

--- README.md
+++
@@ -1,5 +0,0 @@
-# OpenAI Image Generator
-
-We can get the Documentation on [Github repository for OpenAI][1]
-
-[1]: https://github.com/openai/openai-node
--- routes/openaiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const res = require("express/lib/response");
-const { generateImage } = require("../controllers/openaiController");
-const router = express.Router();
-
-router.post("/generateimage", generateImage);
-
-module.exports = router;
